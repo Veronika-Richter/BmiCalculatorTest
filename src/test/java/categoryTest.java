25917afd85496b06bd7c65be0139968d1ce93454
Veronika-Richter <veronikaveronika51@gmail.com>
2020-02-23 15:19:13 +0300

    categoryTestOverweight

--- src/test/java/categoryTest.java
+++ src/test/java/categoryTest.java
@@ -23,4 +23,17 @@ public class categoryTest {
         assertEquals(category, "Your category is Normal", "Категория неверная");
         browser.quit();
     }
+
+    @Test
+    public void categoryOverweight() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://healthunify.com/bmicalculator/");
+        browser.findElement(By.name("wg")).sendKeys("80");
+        browser.findElement(By.name("ht")).sendKeys("175");
+        browser.findElement(By.name("cc")).click();
+        String category = browser.findElement(By.name("desc")).getAttribute("value");
+        assertEquals(category, "Your category is Overweight", "Категория неверная");
+        browser.quit();
+    }
 }
